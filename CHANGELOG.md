9a74ed47f2ddf5aae44659041ce629c7b928fc81
FrazileDevelopers <frazile.com@gmail.com>
2019-01-08 23:18:24 +0530

    Frazile Algorithm

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
 ## 0.0.1 - January 4, 2019.
 
-* Facemash Algorithm realeased.
+* Facemash Algorithm released.
+
+## 0.0.2 – January 5, 2019.
+
+* Facemash Algorithm bugs fixed.
+
+## 0.0.3 – January 9, 2019.
+
+* Added pi funtion which returns PI value.
--- lib/frazile_algorithm.dart
+++ lib/frazile_algorithm.dart
@@ -1,23 +1,24 @@
 library frazile_algorithm;
+
 import 'dart:math';
 
-/* 
-  This is the class of Algorithms.
-*/
-class Frazile_Algo {
-  /* 
-    The Facemash Ranking Algorithm from The Social Network Movie.
-  */
+// This is the class of Algorithms.
 
-  // Get the Expected Value
+class frazilealgo {
+  // The Facemash Ranking Algorithm from The Social Network Movie.
+
+  /// Get the Expected Value;
   double facemash_expected(double $Ra, double $Rb) =>
       (1 / (1 + pow(10, ($Rb - $Ra) / 400)));
 
-  // Get the Win Score
+  /// Get the Win Score;
   double facemash_win(int $score, double $expected, [int $k = 24]) =>
       ($score + $k * (1 - $expected));
 
-  // Get the Loss Score;
+  /// Get the Loss Score;
   double facemash_loss(int $score, double $expected, [int $k = 24]) =>
       ($score + $k * (0 - $expected));
+
+  /// Get the PI value;
+  double pi() => pi();
 }
